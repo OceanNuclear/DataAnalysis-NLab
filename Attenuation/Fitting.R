7b5e4a9f70b3df4a0a69c1e2c8fa63ea9e347a0b
OceanNuclear <annoyingamygdala@gmail.com>
2019-01-01 20:41:35 +0000

    Finalized the plotting code

--- Attenuation/Fitting.R
+++ Attenuation/Fitting.R
@@ -1,8 +1,5 @@
 #!/usr/bin/Rscript
 library(ggplot2)
-#library(dplyr, warn.conflicts = F)
-#library(devtools)
-library(reshape2)
 quadrature <- function(a,b){
 	return (sqrt(a^2+b^2))
 }
@@ -37,16 +34,12 @@ readVar <- function(fname,color){
 	row.names(df)<-NULL
 	return(df)
 }
-toFormula<- function(dat){#You'd think that given R is a plotting engine, I don't have to write my own code just to add numbers onto the graph...
-	#stopifnot(length(coefs)==2)#assert that it has two coefficients
-	m=1
-	c=1
-	dm=1
-	dc=1
-	text <-paste("m= ",toString(m))
-	text <-paste("c= ",toString(c))
-	text <-paste("%+-%",toString(dm))
-	text <-paste("dc=",toString(dc))
+toFormula<- function(coefs,srpcov){
+	#You'd think for a language specialzing in plotting, I don't have to write my own code just to add numbers onto the graph...	
+	text <-paste(     "m=", toString(signif(coefs[2],3)))
+	text <-paste(text,"±" , toString(signif(srpcov[2,2],3)))
+	text <-paste(text,",c=",toString(signif(coefs[1],3)))
+	text <-paste(text,"±" , toString(signif(srpcov[1,1],3)))
 	return (text)
 }
 lmize <- function(dat,resid=F,cov=F){
@@ -62,10 +55,10 @@ lmize <- function(dat,resid=F,cov=F){
 		{return (pcov)}
 	srpcov	<- sqrt(abs(pcov))#square root of covariance materix
 	optimized=data.frame(
-		m=coefs[1],c=coefs[2],
-		dm = srpcov[1,1],dc = srpcov[2,2],
+		m=coefs[2],c=coefs[1],
+		dm = srpcov[2,2],dc = srpcov[1,1],
 		chi.sq.per.DoF= summary(lmobj)$sigma,#chi-squared per DoF
-		formula=toFormula(coefs),
+		formula=toFormula(coefs,srpcov),
 		color=color
 		)
 	return (optimized)
@@ -79,14 +72,15 @@ dfc		=  readVar(  "Col.tsv",  c.conv[1,1])
 opt.c	<- lmize(dfc)
 dfu = readVar("Uncol.tsv",  c.conv[2,1])
 opt.u	<- lmize(dfu)
-print(opt.u)
+
+DF = rbind(dfc,dfu)#combine the data frames
 #create the caption
-caption = paste('collimated fit : m=',toString(opt.c['c'])," c=",toString(opt.c["m"]))
-caption <- paste(caption,"\nuncollimated fit:m=")
-caption <- paste(caption,toString(opt.u['c'])," c=",toString(opt.u["m"]))
-text = "hello"
-DF = rbind(dfc,dfu)
-#combined the dataframes, plot
+caption="collimated:"
+caption=paste(caption, toString(unlist(opt.c['formula'])))
+caption=paste(caption, '\nuncollimated:')
+caption=paste(caption, toString(unlist(opt.u['formula'])))
+
+#plotting...
 gplotobj<-
 	#plot and specify dot size
 	ggplot(DF,aes(x=thickness,y=log.dose.rate,color=color))+ geom_point(size = 2)+
@@ -97,28 +91,9 @@ gplotobj<-
 		title="Semi-log(y) plot of dose rate")+
 	#change the name of colors on the legedn into things
 	scale_color_manual(labels = c.conv[,2], values = c.conv[,1]) 
-print('reached this point')
 gplotobj<-gplotobj+
-	geom_abline(data=opt.c,aes(intercept=m,slope=c,color=color))+
+	geom_abline(data=opt.c,aes(intercept=c,slope=m,color=color))+
 	#stat_smooth_func(geom="text",method="lm",hjust=0,parse=TRUE) +
-	geom_abline(data=opt.u,aes(intercept=m,slope=c,color=color))+
-	labs(caption=toString(opt.c['formula']))
-print(opt.c['formula'][1])
-#The act of saving is quite simple:)
-gplotobj
-#gplotobj+geom_abline(data=opt.u,aes(intercept=m,slope=c,color=color))
-
-#plot(dose.rate ~ thickness, data=dfr, ylab="Dose rate(microSv/hr)", xlab="thickness(mm)",col="blue")
-	if(F){
-		thickness= dat["thickness"]
-		y 		 = dat["log.dose.rate"]
-		yfit 	 = equation(thickness,coefs[1],coefs[2])
-		dy		 = dat["error"]
-		resid	<- y-yfit
-	}
-	if (F){
-		equation <- function(x,c,m){
-		answer = exp(c)*exp(m*x)
-		return (log(answer))
-	}
-	}
+	geom_abline(data=opt.u,aes(intercept=c,slope=m,color=color))+
+	labs(caption=caption)
+gplotobj #The command for saving is incredibly simple:)
