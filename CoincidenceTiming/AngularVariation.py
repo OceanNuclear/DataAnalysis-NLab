fef39b5def7ca530ce5f5e7ed4aef922a7b591ed
OceanNuclear <annoyingamygdala@gmail.com>
2019-03-24 17:39:18 +0000

    Made more comments for easier understanding

--- CoincidenceTiming/AngularVariation.py
+++ CoincidenceTiming/AngularVariation.py
@@ -2,11 +2,12 @@
 from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
 from matplotlib import pyplot as plt
 
-a=2.95
-d=7.62/2
-h=sqrt(a**2+d**2)
+d=10
+a=7.62/2
+h=sqrt(a**2+d**2)#hypotenuse of the right angle triangle formed by a and d.
 numTests=200000
 
+#outdated version, replaced by the subprogram distList below.
 # def distance(u,v):
 # 	#finds the distance between two vectors, u and v
 # 	difference = ary(u-v)
@@ -14,8 +15,8 @@ numTests=200000
 # 	return answer
 
 def distList(uList,vList):
-	# assert np.shape(uList)[0]==3
-	# assert np.shape(vList)[0]==3
+	# assert np.shape(uList)[0]==3 #tests to check that inputs were of the right shapes.
+	# assert np.shape(vList)[0]==3	#These test were disabled after debugging has finished.
 	difference=uList-vList
 	diffsq= np.square(difference)
 	diffsqsum=np.sum(diffsq,axis=0)
@@ -23,6 +24,7 @@ def distList(uList,vList):
 	distanceList=sqrt(diffsqsum)
 	return distanceList
 
+#outdated version, replaced by the subprogram InCircleList below.
 # def InCircle(theta,phi,centroid_phi_displacement):
 # 	#check if the point (theta, phi) falls inside the circle or not.
 # 	r0=spherical_cartesian(pi/2,centroid_phi_displacement)
@@ -35,20 +37,22 @@ def distList(uList,vList):
 # 		return False
 
 def InCircleList(thetaList,phiList,centroid_phi_displacement):
+	#thetaList and phiList are expected to be of the same shape.
 	numList= len(thetaList)
-	pi2    = pi/2*np.ones(numList)
+	pi2    = pi/2*np.ones(numList)	#lengthen it into a list of the same shape
 	PHIDISP= centroid_phi_displacement*np.ones(numList)
 	
 	r0List=spherical_cartesian(pi2,PHIDISP)
 	r1List=spherical_cartesian(thetaList,phiList)
 	r_max =sqrt(2)* sqrt(1-d/h)#which should be a scalar
+	#The formula for r_max is analytically derived; derivation not included in this code.
 
 	distanceList=distList(r0List,r1List)
 	TruthArray=np.less_equal(distanceList,r_max)
 	return TruthArray
 
 def Overlap(THETA):
-	#THETA is the angle separating the centre of the two circles.
+	#THETA is the angle separating the centre of the two 'circles'.
 	sumOverlap=0
 	thetaList,phiList=[],[]
 	
@@ -57,7 +61,7 @@ def Overlap(THETA):
 		phiList.append(rnPhi())
 	thetaList,phiList=ary([thetaList, phiList])
 	
-	TruthArray1=InCircleList(thetaList,phiList,0)
+	TruthArray1=InCircleList(thetaList,phiList,0)#multiplication of the two boolean arrays is equilvalent to using the "and" condition
 	TruthArray2=InCircleList(thetaList,phiList,THETA)
 	TotalValidAnswer=TruthArray1*TruthArray2
 
