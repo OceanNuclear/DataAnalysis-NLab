f56cc6d941fb53a84310f12cbf1c31a63f822909
OceanNuclear <annoyingamygdala@gmail.com>
2019-03-24 17:23:57 +0000

    Optimized speed of program to calculate angular variation of coincidence rate

--- CoincidenceTiming/AngularVariation.py
+++ CoincidenceTiming/AngularVariation.py
@@ -3,36 +3,65 @@ from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import arr
 from matplotlib import pyplot as plt
 
 a=2.95
-d=7.62
+d=7.62/2
 h=sqrt(a**2+d**2)
-numTests=20000
+numTests=200000
 
-def distance(u,v):
-	#finds the distance between two vectors, u and v
-	difference = ary(u-v)
-	answer = sqrt(np.sum(difference**2))
-	return answer
+# def distance(u,v):
+# 	#finds the distance between two vectors, u and v
+# 	difference = ary(u-v)
+# 	answer = sqrt(np.sum(difference**2))
+# 	return answer
 
-def InCircle(theta,phi,centroid_phi_displacement):
-	#check if the point (theta, phi) falls inside the circle or not.
-	r0=spherical_cartesian(pi/2,centroid_phi_displacement)
-	r1=spherical_cartesian(theta,phi)
-	r_max=sqrt(2)* sqrt(1-d/h)#which should be a scalar
+def distList(uList,vList):
+	# assert np.shape(uList)[0]==3
+	# assert np.shape(vList)[0]==3
+	difference=uList-vList
+	diffsq= np.square(difference)
+	diffsqsum=np.sum(diffsq,axis=0)
+	# assert np.shape(vList)[1]==len(diffsqsum)
+	distanceList=sqrt(diffsqsum)
+	return distanceList
+
+# def InCircle(theta,phi,centroid_phi_displacement):
+# 	#check if the point (theta, phi) falls inside the circle or not.
+# 	r0=spherical_cartesian(pi/2,centroid_phi_displacement)
+# 	r1=spherical_cartesian(theta,phi)
+# 	r_max=sqrt(2)* sqrt(1-d/h)#which should be a scalar
 	
-	if distance(r0,r1)<=r_max:
-		return True
-	else:
-		return False
+# 	if distance(r0,r1)<=r_max:
+# 		return True
+# 	else:
+# 		return False
+
+def InCircleList(thetaList,phiList,centroid_phi_displacement):
+	numList= len(thetaList)
+	pi2    = pi/2*np.ones(numList)
+	PHIDISP= centroid_phi_displacement*np.ones(numList)
+	
+	r0List=spherical_cartesian(pi2,PHIDISP)
+	r1List=spherical_cartesian(thetaList,phiList)
+	r_max =sqrt(2)* sqrt(1-d/h)#which should be a scalar
+
+	distanceList=distList(r0List,r1List)
+	TruthArray=np.less_equal(distanceList,r_max)
+	return TruthArray
 
 def Overlap(THETA):
-	#THETA is the angele separating the centre of the two circles.
+	#THETA is the angle separating the centre of the two circles.
 	sumOverlap=0
+	thetaList,phiList=[],[]
+	
 	for i in range (numTests):
-		testTheta=rnTheta()
-		testPhi  =  rnPhi()
-		if InCircle(testTheta,testPhi,0) and InCircle(testTheta,testPhi,THETA):
-			sumOverlap+=1
-	return sumOverlap/numTests
+		thetaList.append(rnTheta())
+		phiList.append(rnPhi())
+	thetaList,phiList=ary([thetaList, phiList])
+	
+	TruthArray1=InCircleList(thetaList,phiList,0)
+	TruthArray2=InCircleList(thetaList,phiList,THETA)
+	TotalValidAnswer=TruthArray1*TruthArray2
+
+	return sum(TotalValidAnswer)/numTests
 
 def rnTheta():
 	u=np.random.uniform(-1,1)
@@ -41,24 +70,19 @@ def rnTheta():
 def rnPhi():
 	return np.random.uniform(0,tau)
 
-def cartesian_spherical(x, y, z):
-	x,y,z = ary([x,y,z], dtype=float) #change the data type to the desired format
-
-	Theta = arccos(np.clip(z,-1,1))
-	Phi = arctan(np.divide(y,x))	#This division is going to give nan if (x,y,z) = (0,0,1)
-	Phi = np.nan_to_num(Phi)	#Therefore assert phi = 0 if (x,y) = (0,0)
-	Phi+= ary( (np.sign(x)-1), dtype=bool)*pi #if x is positive, then phi is on the RHS of the circle; vice versa.
-	return ary([Theta, Phi])
-
 def spherical_cartesian(theta, phi):	#simple conversion from spherical to cartesian, assuming r = 1
 	x = sin(theta)*cos(phi)
 	y = sin(theta)*sin(phi)
 	z = cos(theta)
 	return ary([x,y,z])
-y=[]
-for x in range (0,91):
-	y.append(Overlap(np.deg2rad(x)))
-plt.plot(y)
-plt.xlabel("degrees")
-plt.ylabel("fraction of gamma's detected by both detectors")
-plt.show()
+
+if __name__=="__main__":
+	y=[]
+	for x in range (0,91):
+		print("computing for", x,"degree")
+		y.append(Overlap(np.deg2rad(x)))
+	plt.plot(y)
+	plt.xlabel("degrees")
+	plt.ylabel("fraction of "+r"$\gamma$"+"'s detected by both detectors")
+	plt.title("Angular variation by random number simulations")
+	plt.show()
