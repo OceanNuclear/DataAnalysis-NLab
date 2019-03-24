18f682737565bdb903a2826d3c7f3cad0839069b
OceanNuclear <annoyingamygdala@gmail.com>
2019-03-24 16:41:48 +0000

    Created function for checking coincidence rate

---
+++ CoincidenceTiming/AngularVariation.py
@@ -0,0 +1,64 @@
+#!/home/ocean/anaconda3/bin/python3
+from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
+from matplotlib import pyplot as plt
+
+a=2.95
+d=7.62
+h=sqrt(a**2+d**2)
+numTests=20000
+
+def distance(u,v):
+	#finds the distance between two vectors, u and v
+	difference = ary(u-v)
+	answer = sqrt(np.sum(difference**2))
+	return answer
+
+def InCircle(theta,phi,centroid_phi_displacement):
+	#check if the point (theta, phi) falls inside the circle or not.
+	r0=spherical_cartesian(pi/2,centroid_phi_displacement)
+	r1=spherical_cartesian(theta,phi)
+	r_max=sqrt(2)* sqrt(1-d/h)#which should be a scalar
+	
+	if distance(r0,r1)<=r_max:
+		return True
+	else:
+		return False
+
+def Overlap(THETA):
+	#THETA is the angele separating the centre of the two circles.
+	sumOverlap=0
+	for i in range (numTests):
+		testTheta=rnTheta()
+		testPhi  =  rnPhi()
+		if InCircle(testTheta,testPhi,0) and InCircle(testTheta,testPhi,THETA):
+			sumOverlap+=1
+	return sumOverlap/numTests
+
+def rnTheta():
+	u=np.random.uniform(-1,1)
+	return arccos(u) #theta ranges from 0 to pi.
+
+def rnPhi():
+	return np.random.uniform(0,tau)
+
+def cartesian_spherical(x, y, z):
+	x,y,z = ary([x,y,z], dtype=float) #change the data type to the desired format
+
+	Theta = arccos(np.clip(z,-1,1))
+	Phi = arctan(np.divide(y,x))	#This division is going to give nan if (x,y,z) = (0,0,1)
+	Phi = np.nan_to_num(Phi)	#Therefore assert phi = 0 if (x,y) = (0,0)
+	Phi+= ary( (np.sign(x)-1), dtype=bool)*pi #if x is positive, then phi is on the RHS of the circle; vice versa.
+	return ary([Theta, Phi])
+
+def spherical_cartesian(theta, phi):	#simple conversion from spherical to cartesian, assuming r = 1
+	x = sin(theta)*cos(phi)
+	y = sin(theta)*sin(phi)
+	z = cos(theta)
+	return ary([x,y,z])
+y=[]
+for x in range (0,91):
+	y.append(Overlap(np.deg2rad(x)))
+plt.plot(y)
+plt.xlabel("degrees")
+plt.ylabel("size")
+plt.show()
--- CoincidenceTiming/QuickPlotter.py
+++ CoincidenceTiming/QuickPlotter.py
@@ -1,15 +1,16 @@
 #!/home/ocean/anaconda3/bin/python3
 from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
 from matplotlib import pyplot as plt
-fileName = [#"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/11-16/Ascii/133Ba_spectrum.Spe",
-			#"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/11-16/Ascii/BG-subtracted-133Ba_spectrum.Spe",
-			"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/11-23/Calibration/60Co.Spe",
-			"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/11-23/Calibration/137Cs.Spe",
-			"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/11-23/Calibration/152Eu.Spe",
+fileName = [
+			# "/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/12-07/Calibration/Det_A_44Ti_67_78_peaks_calibraton.Spe",
+			# "/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/12-07/Calibration/ShowEnergySelection-cutoff/Det_A_44Ti_67_SCA_Window.Spe",
+			"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/12-07/Calibration/Det_B_44Ti_67_78_peaks_calibraton.Spe",
+			"/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/12-07/Calibration/ShowEnergySelection-cutoff/Det_B_44Ti_67_78_SCA_Window.Spe",
+			# "/home/ocean/Documents/GitHubDir/DataAnalysis-NLab/CoincidenceTiming/11-30/22Na-B-511Window-Bipolar-0.5shaping.Spe",
 			]
 			#I've checked 22Na, 241Am, 44Ti. They all don't need background subtraction.
 n=0
-names = iter(['1','2',"3"])
+names = iter(['before','after',"3","4"])
 for fn in fileName:
 	n+=1
 	f = open(fn,"r")
@@ -17,9 +18,9 @@ for fn in fileName:
 	f.close()
 	data = [ int(x) for x in data[12:2060] ] #cut the file at appropriate points
 	data = data[:]
-	plt.semilogy(data,label= str(n))
-	#plt.semilogy(data,label=next(names))
-plt.title("Spectrum of "+r"${}^{133}$Ba"+" ?")
+	# plt.semilogy(data,)#label= str(n))
+	plt.semilogy(data,label=next(names))
+plt.title("Gate applied onto a spectra of"+r"${}^{22}$Na"+"")
 plt.xlabel('channel')
 plt.ylabel("counts")
 plt.legend()
