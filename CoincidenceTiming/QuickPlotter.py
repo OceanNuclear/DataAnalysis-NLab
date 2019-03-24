e142f91bab89562eabbdd18b859071505968da08
OceanNuclear <annoyingamygdala@gmail.com>
2019-03-24 19:36:59 +0000

    Completed all plotting processes

--- CoincidenceTiming/QuickPlotter.py
+++ CoincidenceTiming/QuickPlotter.py
@@ -2,8 +2,8 @@
 from numpy import cos, arccos, sin, arctan, tan, pi, sqrt; from numpy import array as ary; import numpy as np; tau = 2*pi
 from matplotlib import pyplot as plt
 
-fname="CloseCounts.txt"
-# fname="FarCounts.txt"
+# fname="CloseCounts.txt"
+fname="FarCounts.txt"
 
 f=open(fname,"r")
 data=f.readlines()
@@ -18,8 +18,8 @@ for line in data:
 		z.append(line[2])
 x,y,z=ary([x,y,z],dtype=int)
 plt.plot(x,y/z,marker="x")
-plt.title("Angular variation of count rate at 2.95cm away from the source")
-#plt.title("Angular variation of count rate at 10cm away from the source")
+# plt.title("Angular variation of count rate at 2.95cm away from the source")
+plt.title("Angular variation of count rate at 10cm away from the source")
 plt.xlabel("Angle")
 plt.ylabel("Count rate (per second)")
 plt.show()
